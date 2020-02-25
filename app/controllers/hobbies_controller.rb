e99f3bf80cd1c290e68917b0d285d1a585f6a24d
Sara Boyer <sara.jane.boyer@gmail.com>
2020-02-25 14:43:08 -0700

    hobbies model and controller created

---
+++ app/controllers/hobbies_controller.rb
@@ -0,0 +1,13 @@
+class HobbiesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
+  root "hobbies#index"
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  root to: "home#index"
-
   
+  resources :hobbies
 end
