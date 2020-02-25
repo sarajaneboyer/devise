9f83933721368ba0adede5463d4fe6354bde1666
Sara Boyer <sara.jane.boyer@gmail.com>
2020-02-25 15:27:24 -0700

    hobbies index and controller updated

--- app/controllers/hobbies_controller.rb
+++ app/controllers/hobbies_controller.rb
@@ -1,13 +1,34 @@
 class HobbiesController < ApplicationController
+
   def index
+    @hobbies = current_user.hobbies
   end
 
   def show
+    #BEFORE ACTION
   end
 
   def new
+    @hobby = Hobby.new
+  end
+
+  def create
+    @hobby = current_user.hobbies.new(hobby_params)
+
+    if @hobby.save
+      redirect_to hobbies_path
+    else
+      render :new
+    end
   end
 
   def edit
+    #BEFORE ACTION
+  end
+
+  private
+
+  def hobby_params
+    params.require(:hobby).permit(:name)
   end
 end
